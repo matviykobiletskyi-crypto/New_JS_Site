f8b1f93275d60605e96ad2830c65ba4f57004aab
matviykobiletskyi-crypto <matviykobiletskyi@gmail.com>
2026-05-20 22:23:24 +0300

    final

--- js/products.js
+++ js/products.js
@@ -1,36 +1,229 @@
 var products = [
     {
         id: 1,
-        name: "Товар 1",
-        category: "Категорія 1",
-        price: 1000,
-        emoji: "😊",
+        name: "Apple iPhone 17 Pro Max 256GB (Cosmic Orange)",
+        category: "iPhone",
+        price: "66 999 грн",
+        emoji: "📱",
         desc: "Опис товару 1"
     },
-    {
+        {
         id: 2,
-        name: "Товар 2",
-        category: "Категорія 1",
-        price: 1000,
-        emoji: "😊",
+        name: "Apple iPhone 17 Pro 256GB (Silver)",
+        category: "iPhone",
+        price: "63 999 грн",
+        emoji: "📱",
+        desc: "Опис товару 1"
+    },
+        {
+        id: 3,
+        name: "Apple iPhone 17 Pro 256GB (Deep Blue)",
+        category: "iPhone",
+        price: "61 999 грн",
+        emoji: "📱",
+        desc: "Опис товару 1"
+    },
+        {
+        id: 4,
+        name: "Apple iPhone 17 Air 256GB (Space Black) (e-Sim)",
+        category: "iPhone",
+        price: "42 399 грн",
+        emoji: "📱",
+        desc: "Опис товару 1"
+    },
+        {
+        id: 5,
+        name: "Apple iPhone 17 256GB (Black)",
+        category: "iPhone",
+        price: "41 699 грн",
+        emoji: "📱",
+        desc: "Опис товару 1"
+    },
+
+
+
+
+
+
+
+    {
+        id: 6,
+        name: "Apple Watch Series 11 GPS, 42mm Rose Gold Aluminum Case with Light Blush Sport Band (S/M) (MEU04)",
+        category: "Apple Watch",
+        price: "16 599 грн",
+        emoji: "⌚",
         desc: "Опис товару 2"
     },
+        {
+        id: 7,
+        name: "Apple Watch Series 11 GPS, 46mm Jet Black Aluminum Case with Black Sport Band (M/L) (MEUX4)",
+        category: "Apple Watch",
+        price: "17 299 грн",
+        emoji: "⌚",
+        desc: "Опис товару 2"
+    },
+        {
+        id: 8,
+        name: "Apple Watch SE 3 GPS, 40mm Starlight Aluminum Case with Starlight Sport Band (S/M) (MEH34)",
+        category: "Apple Watch",
+        price: "12 599 грн",
+        emoji: "⌚",
+        desc: "Опис товару 2"
+    },
+        {
+        id: 9,
+        name: "Apple Watch Series 11 GPS, 46mm Rose Gold Aluminum Case with Light Blush Sport Band (M/L) (MEV74)",
+        category: "Apple Watch",
+        price: "17 499 грн",
+        emoji: "⌚",
+        desc: "Опис товару 2"
+    },
+        {
+        id: 10,
+        name: "Apple Watch Series 10 GPS, 46mm Jet Black Aluminum Case with Black Sport Band (M/L) (MWWQ3)",
+        category: "Apple Watch",
+        price: "16 499 грн",
+        emoji: "⌚",
+        desc: "Опис товару 2"
+    },
+
+
+
+
+
+
     {
-        id: 3,
-        name: "Товар 3",
-        category: "Категорія 3",
-        price: 1000,
-        emoji: "😊",
+        id: 11,
+        name: "AirPods Pro 2 with MagSafe Case (USB‑C) (MTJV3) (2023)",
+        category: "Apple AirPods",
+        price: "9 599 грн",
+        emoji: "🎧",
         desc: "Опис товару 3"
     },
+        {
+        id: 12,
+        name: "Навушники Apple AirPods 4 with Active Noise Cancellation (MXP93) (2024)",
+        category: "Apple AirPods",
+        price: "7 999 грн",
+        emoji: "🎧",
+        desc: "Опис товару 3"
+    },
+        {
+        id: 13,
+        name: "Навушники Apple AirPods 4 (2024)",
+        category: "Apple AirPods",
+        price: "6 699 грн",
+        emoji: "🎧",
+        desc: "Опис товару 3"
+    },
+        {
+        id: 14,
+        name: "Навушники Apple AirPods Pro 3 (MFHP4) (2025)",
+        category: "Apple AirPods",
+        price: "10 799 грн",
+        emoji: "🎧",
+        desc: "Опис товару 3"
+    },
+        {
+        id: 15,
+        name: "AirPods Pro 2 with MagSafe Case (USB‑C) (MTJV3) (2023) (Ultra)",
+        category: "Apple AirPods",
+        price: "10 399 грн",
+        emoji: "🎧",
+        desc: "Опис товару 3"
+    },
+
+
+
+
+
+
     {
-        id: 4,
-        name: "Товар 4",
-        category: "Категорія 4",
-        price: 1000,
-        emoji: "😊",
+        id: 16,
+        name: "Apple iPad Pro 11 2025, 256GB, Space Black, Wi-Fi (M5) (MDWK4)",
+        category: "Apple iPad",
+        price: "46 199 грн",
+        emoji: "📱",
         desc: "Опис товару 4"
     },
+        {
+        id: 17,
+        name: "Apple iPad Pro 11 2025, 256GB, Silver, Wi-Fi (M5) (MDWL4)",
+        category: "Apple iPad",
+        price: "46 799 грн",
+        emoji: "📱",
+        desc: "Опис товару 4"
+    },
+        {
+        id: 18,
+        name: "Apple iPad Pro 13 2025, 256GB, Space Black, Wi-Fi (M5) (MDYJ4)",
+        category: "Apple iPad",
+        price: "58 599 грн",
+        emoji: "📱",
+        desc: "Опис товару 4"
+    },
+        {
+        id: 19,
+        name: "Apple iPad 11 128GB, Wi-Fi (Silver) (2025) (MD3Y4)",
+        category: "Apple iPad",
+        price: "17 199 грн",
+        emoji: "📱",
+        desc: "Опис товару 4"
+    },
+        {
+        id: 20,
+        name: "Apple iPad Pro 13 2025, 256GB, Silver, Wi-Fi (M5) (MDYK4)",
+        category: "Apple iPad",
+        price: "58 599 грн",
+        emoji: "📱",
+        desc: "Опис товару 4"
+    },
+
+
+
+
+            {
+        id: 21,
+        name: "MacBook Air 13 Retina, Sky Blue, 256GB, 10 CPU / 8 GPU, 16GB RAM with Apple M4 (2025) (MC6T4)",
+        category: "Apple Mac",
+        price: "50 599 грн",
+        emoji: "💻",
+        desc: "Опис товару 5"
+    },
+                {
+        id: 22,
+        name: "MacBook Neo 13 Retina, Blush, 256GB, 6 CPU / 5 GPU, 8GB RAM with Apple A18 Pro (2026) (MHFH4)",
+        category: "Apple Mac",
+        price: "32 299 грн",
+        emoji: "💻",
+        desc: "Опис товару 5"
+    },
+                {
+        id: 23,
+        name: "MacBook Air 13 Retina, Midnight, 256GB, 10 CPU / 8 GPU, 16GB RAM with Apple M4 (2025) (MW123)",
+        category: "Apple Mac",
+        price: "50 599 грн",
+        emoji: "💻",
+        desc: "Опис товару 5"
+    },
+                {
+        id: 24,
+        name: "MacBook Neo 13 Retina, Silver, 256GB, 6 CPU / 5 GPU, 8GB RAM with Apple A18 Pro (2026) (MHFA4)",
+        category: "Apple Mac",
+        price: "32 299 грн" ,
+        emoji: "💻",
+        desc: "Опис товару 5"
+    },
+                {
+        id: 25,
+        name: "MacBook Air 15 Retina, Midnight, 256GB, 10 CPU / 10 GPU, 16GB RAM with Apple M4 (2025) (MW1L3)",
+        category: "Apple Mac",
+        price: "54 999 грн",
+        emoji: "💻",
+        desc: "Опис товару 5"
+    },
+    
+    
 ];
 
 function getProduct(id) {
